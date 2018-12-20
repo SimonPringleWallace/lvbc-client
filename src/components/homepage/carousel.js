efea65f101caf1568193baa7a1a9d975fd0e9946
SimonPringleWallace <s.pringlewallace@gmail.com>
2018-12-19 20:07:07 -0500

    Add LVBC Specific Images to Carousel
    - create a rough, standard size for all carousel images

--- src/components/homepage/carousel.js
+++ src/components/homepage/carousel.js
@@ -10,26 +10,41 @@ class MyCarousel extends Component {
       interval={6000}
       pauseOnHover={false}
       >
-        <Carousel.Item>
-          <img className='caro-image' alt="1000x500" src={require('../../images/lvbc-1.jpg')} />
+        <Carousel.Item >
+        <div className='image-container'>
+          <img className='caro-image'  alt="1000x500" src={require('../../images/Picture1.jpg')} />
           <Carousel.Caption>
             <h3>Learn to Read</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
+          </div>
         </Carousel.Item>
         <Carousel.Item>
-          <img className='caro-image' alt="1000x500" src={require('../../images/lvbc-2.jpg')} />
+        <div className='image-container'>
+          <img className='caro-image' alt="1000x500" src={require('../../images/Picture2.jpg')} />
           <Carousel.Caption>
             <h3>Help People Learn How to Read</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
+          </div>
         </Carousel.Item>
         <Carousel.Item>
-          <img className='caro-image' alt="1000x500" src={require('../../images/lvbc-3.jpg')} />
+        <div className='image-container'>
+          <img className='caro-image' alt="1000x500" src={require('../../images/IMG_2192.JPG')} />
           <Carousel.Caption>
             <h3>Learn to Help People Learn How to Read</h3>
             <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
           </Carousel.Caption>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+        <div className='image-container'>
+          <img className='caro-image' alt="1000x500" src={require('../../images/Picture4.jpg')} />
+          <Carousel.Caption>
+            <h3>Learn to Help People Learn How to Read</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+          </div>
         </Carousel.Item>
       </Carousel>
       </div>
