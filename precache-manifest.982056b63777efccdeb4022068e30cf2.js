2dc6f36fda8e53f31d69c681a1901c8b0162bcca
SimonPringleWallace <s.pringlewallace@gmail.com>
2019-03-03 17:52:36 -0500

    deploy task

---
+++ precache-manifest.982056b63777efccdeb4022068e30cf2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3c8754976b53d2a98391fc3e117a48d",
+    "url": "/lvbc-client/static/media/hero-book.b3c87549.jpg"
+  },
+  {
+    "revision": "224a8fc6c8f4248946078f1759f2f1f9",
+    "url": "/lvbc-client/static/media/adult-reader-40.224a8fc6.jpg"
+  },
+  {
+    "revision": "74da33af08a7dbdf2e6d748141d62076",
+    "url": "/lvbc-client/static/media/Logo-horizontal-red.74da33af.png"
+  },
+  {
+    "revision": "5ae0f86555c4e6b0e0e6",
+    "url": "/lvbc-client/static/js/runtime~main.5ae0f865.js"
+  },
+  {
+    "revision": "19aedc1f7f5cdb7ed219",
+    "url": "/lvbc-client/static/js/main.19aedc1f.chunk.js"
+  },
+  {
+    "revision": "48e7f56e691d1113d6c1",
+    "url": "/lvbc-client/static/js/1.48e7f56e.chunk.js"
+  },
+  {
+    "revision": "19aedc1f7f5cdb7ed219",
+    "url": "/lvbc-client/static/css/main.68e7d01c.chunk.css"
+  },
+  {
+    "revision": "5d0bd161aace49aa8d32fb73d89d85cc",
+    "url": "/lvbc-client/index.html"
+  }
+];
